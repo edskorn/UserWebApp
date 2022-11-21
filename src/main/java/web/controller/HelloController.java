2d9eaaf52e1338603e4016b0c88d66ff3e1f487c
edskorn <ed.skorn@gmail.com>
2022-11-22 00:28:14 +0700

    added @ModelAttribute("user")

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -3,7 +3,9 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.model.User;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
@@ -24,18 +26,20 @@ public class HelloController {
 
 	@GetMapping(value = "/")
 	public String askUser(ModelMap model) {
-//		List<String> messages = new ArrayList<>();
-//		messages.add("Hello!");
-//		messages.add("I'm Spring MVC application");
-//		messages.add("5.2.0 version by sep'19 ");
-//		model.addAttribute("messages", messages);
+		model.addAttribute("user", new User());
 		return "ask-user";
 	}
 
 	@GetMapping(value = "/showDetails")
-	public String showDetails(@RequestParam(name = "userName") String usrName, ModelMap model) {
-		model.addAttribute("showName", usrName);
+	public String showDetails(@ModelAttribute("user") User user) {
+		//model.addAttribute("showName", usrName);
+		user.setName("Mr. " + user.getName());
 		return "show-details";
 	}
+//	@GetMapping(value = "/showDetails")
+//	public String showDetails(@RequestParam(name = "userName") String usrName, ModelMap model) {
+//		model.addAttribute("showName", usrName);
+//		return "show-details";
+//	}
 	
 }
