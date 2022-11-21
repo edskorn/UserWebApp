48bcd26dce7b7c2f13b99ebd2243d545ed6e3a67
edskorn <ed.skorn@gmail.com>
2022-11-21 22:30:40 +0700

    added simple form userName with controller

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -3,14 +3,16 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/")
+	@GetMapping(value = "/hello")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
 		messages.add("Hello!");
@@ -18,6 +20,22 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/")
+	public String askUser(ModelMap model) {
+//		List<String> messages = new ArrayList<>();
+//		messages.add("Hello!");
+//		messages.add("I'm Spring MVC application");
+//		messages.add("5.2.0 version by sep'19 ");
+//		model.addAttribute("messages", messages);
+		return "ask-user";
+	}
+
+	@GetMapping(value = "/showDetails")
+	public String showDetails(@RequestParam(name = "userName") String usrName, ModelMap model) {
+		model.addAttribute("showName", usrName);
+		return "show-details";
 	}
 	
 }
